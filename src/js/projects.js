c3e21398c6f916b5a9b1f179feb1ca2b13e3a997
cryptotoad.eth <0xcryptotoad@gmail.com>
2022-10-06 21:19:11 -0700

    work on portfolio and styling of projects

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,1 +1,23 @@
 import './../css/style.css';
+
+//Contact button
+var modal = document.getElementById("contactModal");
+
+var btn = document.getElementById("contactBtn");
+
+var span = document.getElementsByClassName("close") [0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
